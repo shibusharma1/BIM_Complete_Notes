6d56cc9f8328c57bb109ee70b4512d91e200839b
Shibu Sharma <168130059+shibusharma1@users.noreply.github.com>
2024-10-06 21:35:08 +0545

    note

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Notes</title>
+</head>
+<body>
+    <p>This is the Repo for the complete Note of the BIM</p>
+    
+</body>
+</html>
